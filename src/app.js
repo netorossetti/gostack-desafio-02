d65c30330bc43002e8d7e99ac4554a89619afa71
Neto Rossetti <netinho.rossetti@gmail.com>
2020-10-28 15:50:22 -0300

    fet: validação de ID nas rotas /repositories/:id via widdleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,16 +11,40 @@ app.use(cors());
 
 const repositories = [];
 
+/** Middleware */
+function validateRepositoryId(request, response, next){
+  const { id } = request.params
+  if(!isUuid(id)){
+    return response.status(400).json({error: "Id is not valid."})
+  }
 
+  const repositoryIndex =  repositories.findIndex( repo => repo.id === id)
+  if( repositoryIndex === -1 ){
+    return response.status(400).json({erro: "Repository dos not exists!"})
+  }
+
+  /* Executa o proximo widdlewares */
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId)
+
+// /** Functions */
+// function getIndexRepositoriesById(id){
+//   return repositories.findIndex( repo => repo.id === id)
+// }
+
+
+
+
+
+/** Rotas */
 app.get("/repositories", (request, response) => {
- 
   return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
- 
   const { title, url, techs } = request.body
-  
   const repository = {
     id: uuid(),
     title,
@@ -28,22 +52,14 @@ app.post("/repositories", (request, response) => {
     techs,
     likes: 0,
   }
-
   repositories.push(repository)
-
   return response.status(201).json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params
   const { title, url, techs } = request.body
-  
   const repositoryIndex = repositories.findIndex( repo => repo.id === id)
-
-  if( repositoryIndex === -1 ){
-    return response.status(400).json({erro: "Repository dos not exists!"})
-  }
-
   const repository = {
     id,
     title,
@@ -51,34 +67,21 @@ app.put("/repositories/:id", (request, response) => {
     techs,
     likes: repositories[repositoryIndex].likes,
   };
-
   repositories[repositoryIndex] = repository
-
   return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params
   const repositoryIndex = repositories.findIndex( repo => repo.id === id)
-  if( repositoryIndex === -1 ){
-    return response.status(400).json({erro: "Repository dos not exists!"})
-  }
-
   repositories.splice(repositoryIndex, 1)
   return response.status(204).send()
-
 });
 
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params
-
   const repositoryIndex = repositories.findIndex( repo => repo.id === id)
-  if( repositoryIndex === -1 ){
-    return response.status(400).json({erro: "Repository dos not exists!"})
-  }
-
   repositories[repositoryIndex].likes ++
-
   return response.json(repositories[repositoryIndex])
 });
 
